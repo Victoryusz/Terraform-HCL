256892d5f56edf71c7f891ab1e77f938777fbc5c
Victoryusz <hugo4strong@gmail.com>
2025-04-07 22:56:02 -0300

    Criação de estrutura básica de rede VPC (VPC, Subnet, IGW, Route Table, Security Group) (Backend-S3)

---
+++ aws-vpc/outputs.tf
@@ -0,0 +1,12 @@
+### 
+output "subnet_id" {
+  description = "ID da Subnet criada na AWS"
+  value       = aws_subnet.subnet.id
+}
+
+### 
+output "security_group_id" {
+  description = "ID da security group criada na AWS"
+  value       = aws_security_group.security_group.id
+}
+
