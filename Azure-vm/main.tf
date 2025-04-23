97e783c5a632edc0ddaa5a2d4a166b374cd22dd8
Victoryusz <hugo4strong@gmail.com>
2025-04-23 01:18:33 -0300

    Criação de VM na Azure acessando VNet com Terraform Remote State

---
+++ Azure-vm/main.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_version = ">= 1.3.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.49.0"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "grp_rec_terraform-state"
+    storage_account_name = "victorstate"
+    container_name       = "container-remote-state"
+    key                  = "azure-vm/terraform.tfstate"
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+data "terraform_remote_state" "vnet" {
+  backend = "azurerm"
+  config = {
+    resource_group_name  = "grp_rec_terraform-state"
+    storage_account_name = "victorstate"
+    container_name       = "container-remote-state"
+    key                  = "azure-vnet/terraform.tfstate"
+  }
+}
+
+
---
+++ Azure-vm/variables.tf
@@ -0,0 +1,8 @@
+############### VARIAVEIS ###############
+variable "location" {
+  description = "Região onde os recursos serão criados na Azure"
+  type        = string
+  default     = "West Europe"
+
+}
+########################################
